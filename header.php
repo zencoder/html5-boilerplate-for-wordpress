98a70b833f8f80a14df0392537244fffe7bba537
walker <myself@walkerhamilton.com>
2011-04-25 14:58:22 -0400

    Updated for 1.5.2

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
 
   <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-  <script>!window.jQuery && document.write('<script src="<?php echo $GLOBALS["TEMPLATE_RELATIVE_URL"] ?>html5-boilerplate/js/libs/jquery-1.5.1.min.js"><\/script>')</script>
+  <script>!window.jQuery && document.write('<script src="<?php echo $GLOBALS["TEMPLATE_RELATIVE_URL"] ?>html5-boilerplate/js/libs/jquery-1.5.2.min.js"><\/script>')</script>
 
   <?php wp_head(); ?>
 
