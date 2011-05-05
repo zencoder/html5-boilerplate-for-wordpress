45da73039b464fe89dd9c1325f0cf53316d4d1ae
walker <myself@walkerhamilton.com>
2011-05-04 23:20:48 -0400

    Fixed some more items to be in line with html5-boilerplate

--- header.php
+++ header.php
@@ -11,28 +11,20 @@
 <!--[if IE 8]>    <html class="no-js ie8" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <!-- www.phpied.com/conditional-comments-block-downloads/ -->
-  <!--[if IE]><![endif]-->
+    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+         Remove this if you use the .htaccess -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-       Remove this if you use the .htaccess -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <!-- Does not currently validate. Known issue with the Boilerplate. -->
 
   <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <!--  Mobile Viewport Fix
-        j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag
-  device-width : Occupy full width of the screen in its current orientation
-  initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-  maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
-  -->
-  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
-
+  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
   <!-- Place favicon.ico and apple-touch-icon.png in the root of your domain and delete these references -->
   <link rel="shortcut icon" href="/favicon.ico">
   <link rel="apple-touch-icon" href="/apple-touch-icon.png">
@@ -40,19 +32,18 @@
   <!-- CSS : implied media="all" -->
   <?php versioned_stylesheet(get_bloginfo('template_url')."/style.css") ?>
 
-  <!-- For the less-enabled mobile browsers like Opera Mini -->
-  <?php versioned_stylesheet(get_bloginfo('template_url')."/handheld.css", 'media="handheld"') ?>
+  <!-- Uncomment if you are specifically targeting less enabled mobile browsers
+  <?php versioned_stylesheet(get_bloginfo('template_url')."/handheld.css", 'media="handheld"') ?> -->
 
-  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+  <!-- (Almost) All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
   <?php versioned_javascript(get_bloginfo('template_url')."/js/libs/modernizr-1.7.min.js") ?>
+
+  <!-- Grab Google CDN's jQuery. fall back to local if necessary, we're doing it here, rather than footer, so that plugins have it available when they almost all bind to wp_head() -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+  <script>window.jQuery || document.write("<script src='js/libs/jquery-1.6.min.js'>\x3C/script>")</script>
 
   <!-- Wordpress Head Items -->
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-  <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
-  <script>!window.jQuery && document.write('<script src="<?php bloginfo('template_url') ?>/js/libs/jquery-1.6.min.js"><\/script>')</script>
-
   <?php wp_head(); ?>
 
 </head>
